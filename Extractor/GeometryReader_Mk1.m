f0e236408f47d9ab33ad864f49ae60fb1a4766f1
AstreTunes <mebius.uranus@gmail.com>
2022-06-20 00:21:54 +0200

    Update GeometryReader_Mk1.m

--- Extractor/GeometryReader_Mk1.m
+++ Extractor/GeometryReader_Mk1.m
@@ -22,7 +22,50 @@
     
     %% read header
     
+    numVertexType = byte2Uint32LE(geomCode(1:4));
+    numIndexType = byte2Uint32LE(geomCode(5:8));
+    numVertexBloc = byte2Uint32LE(geomCode(9:12));
+    numIndexBloc = byte2Uint32LE(geomCode(13:16));
+%     numCmodlBloc = byte2Uint32LE(geomCode(17:20));
+%     numArmorBloc = byte2Uint32LE(geomCode(21:24));
     
+    posVertexInfo = byte2Uint32LE(geomCode(25:28));
+    posIndexInfo = byte2Uint32LE(geomCode(33:36));
+    posVertexSection = byte2Uint32LE(geomCode(41:44));
+    posIndexSection = byte2Uint32LE(geomCode(49:52));
+%     posCmodlSection = byte2Uint32LE(geomCode(57:60));
+%     posArmorSection = byte2Uint32LE(geomCode(65:68));
+
+    %% read vertex blocs info
+    
+    infoVertexBloc = {};
+    for indVertexBloc = 1:numVertexBloc
+        cursor = posVertexInfo + (indVertexBloc-1) * 16;
+        infoVertexBloc{indVertexBloc}.nameVertexBloc = byte2HexStr(geomCode(cursor+1:cursor+4));
+        infoVertexBloc{indVertexBloc}.indVertexType = byte2Uint32LE(geomCode(cursor+5:cursor+8));
+        infoVertexBloc{indVertexBloc}.posVertexBloc = byte2Uint32LE(geomCode(cursor+9:cursor+12));
+        infoVertexBloc{indVertexBloc}.numVertex = byte2Uint32LE(geomCode(cursor+13:cursor+16));
+    end
+    
+    %% read index blocs info
+    
+    infoIndexBloc = {};
+    for indIndexBloc = 1:numVertexBloc
+        cursor = posIndexInfo + (indIndexBloc-1) * 16;
+        infoIndexBloc{indIndexBloc}.nameIndexBloc = byte2HexStr(geomCode(cursor+1:cursor+4));
+        infoIndexBloc{indIndexBloc}.indIndexType = byte2Uint32LE(geomCode(cursor+5:cursor+8));
+        infoIndexBloc{indIndexBloc}.posIndexBloc = byte2Uint32LE(geomCode(cursor+9:cursor+12));
+        infoIndexBloc{indIndexBloc}.numIndex = byte2Uint32LE(geomCode(cursor+13:cursor+16));
+    end
+    
+    %% read vertex section
+    
+    % read large vertex blocs info
+    infoLargeVertexBloc = {};
+    for indLargeVertexBloc = 1:numVertexType
+        cursor = posVertexSection + (indLargeVertexBloc-1) * 32;
+        infoLargeVertexBloc{indLargeVertexBloc}.posLargeVertexBloc
+    end
 
 
 % end
